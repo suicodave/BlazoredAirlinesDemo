222dbe9256c83830eb6817381edea8894001f87f
suicodave <suico.dave@gmail.com>
2022-03-24 17:05:50 +0800

    PassengerTable display more columns

---
+++ Pages/Airlines/Airline.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazoredAirlinesDemo.Pages.Airlines
+{
+    public class Airline
+    {
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public string Logo { get; set; }
+
+        public string Slogan { get; set; }
+
+        public string Website { get; set; }
+
+        public string Established { get; set; }
+
+    }
+}
--- Pages/Passengers/Passenger.cs
+++ Pages/Passengers/Passenger.cs
@@ -3,14 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazoredAirlinesDemo.Pages.Airlines;
 
 namespace BlazoredAirlinesDemo.Pages.Passengers
 {
     public class Passenger
     {
+        public string _Id { get; set; } = "";
+
         public string? Name { get; set; }
 
         public int Trips { get; set; }
+
+        public IEnumerable<Airline> Airline { get; set; } = new List<Airline>();
+
+
     }
 
     public class PaginatedPassenger
@@ -19,4 +26,5 @@ namespace BlazoredAirlinesDemo.Pages.Passengers
 
         public IEnumerable<Passenger> Data { get; set; } = new List<Passenger>();
     }
+
 }
