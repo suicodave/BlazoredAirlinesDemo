7531d514fa4c756224c1c89968941fd6082dec90
suicodave <suico.dave@gmail.com>
2022-03-25 18:29:17 +0800

    Passenger: added form dialog

--- Pages/Passengers/PassengerHttpClient.cs
+++ Pages/Passengers/PassengerHttpClient.cs
@@ -12,16 +12,13 @@ namespace BlazoredAirlinesDemo.Pages.Passengers
     public class PassengerHttpClient : IPaginatedHttpClient<Passenger>
     {
         private readonly HttpClient _http;
-        private readonly ILogger<PassengerHttpClient> _logger;
         public PassengerHttpClient(HttpClient http, ILogger<PassengerHttpClient> logger)
         {
-            _logger = logger;
             _http = http;
         }
 
         public async Task<TableData<Passenger>> Paginate(TableState state)
         {
-            _logger.LogInformation("@table", state);
 
             string path = "v1/passenger";
 
